ec9a4302e18732fd2b9432102f87630cd8b3b9f7
Mousa  Rajab <aljawaherimusa@gmail.com>
2021-06-19 09:14:32 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import React, { Component } from 'react';
 
 import Timer from './Timer'
+import Controls from './Controls'
 
+//no need to modify anything in this component
 class App extends Component {
 
-  //no props being used here, so we can use the shorthand declaration of state
   state = {
+    updateInterval: 1,
     timerIDs: []
   }
 
+  componentDidMount() {
+    this.handleAddTimer()
+  }
 
-  //Your code here:
-
-
-
-
-
-
-
-
-
-  // No need to modify anything in render or the class methods below
-  // Unless, of course, you're curious about how it all works
   render() {
-
+    console.log(this.state.timerIDs);
     return (
       <div className="App">
-        <h1>MultiTimer</h1>
-        <button onClick={this.handleAddTimer}>Add New Timer</button>
-
+        <header>
+          <h1>MultiTimer</h1>
+          <Controls updateIntervalSetting={this.updateIntervalSetting} updateInterval={this.state.updateInterval} handleAddTimer={this.handleAddTimer}/>
+        </header>
         <div className="TimerGrid">
           {this.renderTimers()}
         </div>
@@ -38,24 +32,37 @@ class App extends Component {
   }
 
   // returns array of components written in JSX, mapped from this.state.timerIDs
-  renderTimers = () => this.state.timerIDs.map(id => {
-    return <Timer key={id} id={id} removeTimer={this.removeTimer} />
-  })
+  renderTimers = () => this.state.timerIDs.map(({id, updateInterval}) => <Timer key={id} id={id} removeTimer={this.removeTimer} updateInterval={updateInterval}/>)
+
 
   // adds a random number for timer ID
   handleAddTimer = () => {
     this.setState(prevState => ({
-      timerIDs: [...prevState.timerIDs, Math.floor(Math.random()*1000)]
+      timerIDs: [
+        ...prevState.timerIDs,
+        {
+          updateInterval: prevState.updateInterval,
+          id: Date.now()
+        }
+      ]
     }))
   }
 
   // removeTimer updates state, removing any timer that matches the provided author
   removeTimer = id => {
     this.setState(prevState => ({
-      timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
+      timerIDs: prevState.timerIDs.filter(timer => timer.id !== id)
     }))
   }
 
+  updateIntervalSetting = increment => {
+    this.setState(prevState => {
+      if (prevState.updateInterval + increment <= 1) return { updateInterval: 1 }
+      return {
+        updateInterval: prevState.updateInterval + increment
+      }
+    })
+  }
 
 }
 
